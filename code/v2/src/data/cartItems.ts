d36aed11de0b5120868c0d9248a56bfff35caa11
gkkconan <conan.gkk@gmail.com>
2025-06-07 17:50:47 +0200

    chore(cartItems): add id field and improve formatting

--- code/v2/src/data/cartItems.ts
+++ code/v2/src/data/cartItems.ts
@@ -1,57 +1,65 @@
 import type { Product } from "./products";
 
-export interface cartItem extends Product {
+export interface CartItem extends Product {
   quantity: number;
 };
 
-const cartItems: cartItem[] = [
+const cartItems: CartItem[] = [
   {
-      img: "/icons/product-placeholder.svg",
-      name: "Qualità Oro - Caffè d'Altura Grani",
-      price: 11.69,
-      quantity: 1
+    id: 0,
+    name: "Qualità Oro - Caffè d'Altura Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 11.69,
+    quantity: 1
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "¡Tierra! For Amazonia Grani",
-      price: 11.69,
-      quantity: 1
+    id: 1,
+    name: "¡Tierra! For Amazonia Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 11.69,
+    quantity: 1
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "Espresso Barista Intenso Grani",
-      price: 19.99,
-      quantity: 5
+    id: 2,
+    name: "Espresso Barista Intenso Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 19.99,
+    quantity: 5
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "¡Tierra! Wellness Caffè Decerato Grani",
-      price: 23.09,
-      quantity: 3
+    id: 3,
+    name: "¡Tierra! Wellness Caffè Decerato Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 23.09,
+    quantity: 3
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "Qualità Oro Gran Riserva Grani",
-      price: 23.99,
-      quantity: 1
+    id: 4,
+    name: "Qualità Oro Gran Riserva Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 23.99,
+    quantity: 1
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "Espresso Barista Organic Grani",
-      price: 22.99,
-      quantity: 1
+    id: 5,
+    name: "Espresso Barista Organic Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 22.99,
+    quantity: 1
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "Crema Gusto Espresso Classico Grani",
-      price: 8.49,
-      quantity: 2
+    id: 6,
+    name: "Crema Gusto Espresso Classico Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 8.49,
+    quantity: 2
   },
   {
-      img: "/icons/product-placeholder.svg",
-      name: "¡Tierra! For Planet Grani",
-      price: 11.69,
-      quantity: 1
+    id: 7,
+    name: "¡Tierra! For Planet Grani",
+    img: "/icons/product-placeholder.svg",
+    price: 11.69,
+    quantity: 1
   }
 ];
 
