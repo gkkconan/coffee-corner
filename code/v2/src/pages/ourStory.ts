7c4f4b4048d0a19e40b61a5e9a96699786a2521e
gkkconan <conan.gkk@gmail.com>
2025-06-07 02:56:39 +0200

    feat: add our story page

--- code/v2/src/pages/ourStory.ts
+++ code/v2/src/pages/ourStory.ts
@@ -1,3 +1,57 @@
 export function ourStoryPage(): string {
-  return `<h1>Our Story</h1>`;
+  return `
+    <section id="preview" class="center-flex">
+        <span></span>
+        <div class="text">
+            <h1> Our Story </h1>
+            <p>
+                Our story was originally born in a small urban center, among
+                small alleys and skyscrapers, in the heart of the city.
+                It all began as a coffee enthusiast from a young age, James felt
+                the call of coffee and its immensurable aroma. His curiosity to
+                discover the secrets behind each cup led him to travel the world,
+                peering into distant lands and ancient traditions for the key to the
+                perfect coffee.
+            </p>
+        </div>
+    </section>
+
+    <section id="only-text">
+        <h1> A story throught generations </h1>
+        <p>
+            James' passion was passed down from generation to generation,
+            taking root in the heart of the family and guiding it along a path
+            of experimentation and refinement. Each family member left a small
+            mark on the business, enriching it with new discoveries and innovations.
+            Through time, our family has refined production techniques and carefully
+            selected beans from the most remote regions of the world, exploring
+            every corner of the globe in search of the most delicious and authentic coffee.
+        </p>
+    </section>
+
+    <section id="photo-gallery">
+        <h1> Antique shop photos </h1>
+        <div class="container">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </section>
+
+    <section id="text-and-image" class="center-flex">
+        <div class="text">
+            <h1> The shop now </h1>
+            <p>
+                Today, the result of this passion is woven into our cozy store in the center of town.
+                When you enter our space, you will be greeted by the enticing aroma of just-ground coffee.
+                Here you will find our special blends, the result of centuries of experience
+                and dedication to quality. Every cup we serve is a celebration of the art of coffee,
+                a tribute to our history and the passion that leads us every day. Welcome to our world,
+                where every sip is a story to tell.
+            </p>
+        </div>
+        <span></span>
+    </section>
+  `;
 }
