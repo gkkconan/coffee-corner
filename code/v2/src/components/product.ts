1400af72176fb8381e87d1747b22244bb1bc2d63
gkkconan <conan.gkk@gmail.com>
2025-06-12 18:40:44 +0200

    feat: add product link, data-id for buy button, and update layout

--- code/v2/src/components/product.ts
+++ code/v2/src/components/product.ts
@@ -1,7 +1,7 @@
 import type { Product } from "@/data/products";
 import cartIcon from "/icons/cart.svg";
 
-function generateStars(rating: number): string {
+export function generateStars(rating: number): string {
   const maxStars = 5;
   const fullStars = Math.floor(rating);
   const halfStar = (rating - fullStars) >= 0.5 ? 1 : 0;
@@ -18,17 +18,17 @@ function generateStars(rating: number): string {
 export function productCard(product: Product): string {
   return `
     <div class="card center-flex">
-        <span class="img" style="background: url(${product.img}) center/cover no-repeat"></span>
+        <a href="#/product/${product.id}" class="img" style="background: url(${product.img}) center/cover no-repeat"></a>
         <span class="text">
             <h3>${product.name}</h3>
-            <span class="rating-and-price center-flex">
+            <span class="rating-and-price flex">
                 <span class="rating">
                 ${generateStars(product.rating)}
                 </span>
                 <h2>${product.price}€</h2>
             </span>
         </span>
-        <button class="center-grid"> <img src="${cartIcon}" alt="buy" /> </button>
+        <button data-id="${product.id}" class="add-to-cart center-grid"> <img src="${cartIcon}" alt="buy" /> </button>
     </div>
   `;
 }
